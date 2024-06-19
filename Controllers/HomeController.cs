bd904e730f1f00a88cdf61a151a8b3dabc8b2c1d
sohail.ah91@gmail.com <sohail.ah91@gmail.com>
2024-06-19 20:25:57 +0530

    second commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace SampleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            ViewBag.ConnectionString = connectionString;
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,21 +1,43 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+var keyVaultUrl = builder.Configuration["AzureKeyVault:VaultUrl"];
+var secretName = builder.Configuration["AzureKeyVault:SecretName"];
+var managedIdentityClientId = "7d265de3-0ac1-4e81-a054-4f328c42a93c";
+
+// Create a SecretClient using DefaultAzureCredential to retrieve secrets from Key Vault
+var secretClient = new SecretClient(new Uri(keyVaultUrl), new DefaultAzureCredential(new DefaultAzureCredentialOptions
+{
+    ManagedIdentityClientId = managedIdentityClientId
+}));
+
+// Retrieve the secret value from Key Vault
+KeyVaultSecret secret = secretClient.GetSecret(secretName);
+builder.Configuration["ConnectionStrings:DefaultConnection"] = secret.Value;
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
